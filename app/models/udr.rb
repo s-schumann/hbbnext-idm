2d1e213b38ad364e693774b0808018444d33f59d
Sebastian Schumann <seb.schumann@gmail.com>
2012-10-17 21:46:06 +0200

    Added name to cr and udr models

--- app/models/udr.rb
+++ app/models/udr.rb
@@ -1,5 +1,5 @@
 class Udr < ActiveRecord::Base
-  attr_accessible :user_id, :device_id, :cr_ids, :context_ids, :role
+  attr_accessible :user_id, :device_id, :cr_ids, :context_ids, :role, :name
 
   belongs_to :user
   belongs_to :device
