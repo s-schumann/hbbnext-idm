931da7cbafc931f70c17724bce22a5b2145a0ce5
Sebastian Schumann <seb.schumann@gmail.com>
2012-10-17 13:55:34 +0200

    $ rails new . --database=mysql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HbbnextIdm::Application.config.secret_token = '56e1cac7921cbd3e22edb6cd4ad24d040a3c3799ea97ba5f89afd26a4a2f8a1ea8c00f60f2f9c8e198af8c3c9b335309be90d90e33db40394f4fa39aad5a07c2'
