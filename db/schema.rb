3c59fd101a1055b79c3b79a0122418d5f17f2596
Sebastian Schumann <seb.schumann@gmail.com>
2012-10-19 16:39:32 +0200

    Added roles name.
    $ rails generate migration RolesName
    $ rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121019143429) do
+ActiveRecord::Schema.define(:version => 20121019143903) do
 
   create_table "contexts", :force => true do |t|
     t.string   "alias"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(:version => 20121019143429) do
     t.string   "uuid"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "name"
   end
 
   create_table "udrs", :force => true do |t|
