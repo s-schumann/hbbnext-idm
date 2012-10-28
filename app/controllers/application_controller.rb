97905b048ba413ad9037ac5476e8b2f0c7996ac8
Sebastian Schumann <seb.schumann@gmail.com>
2012-10-28 16:36:22 +0100

    Rel. 1.1.0
    Added bootstrap support. Closes #45.
    Reworked CSS. Closes #29.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
   $currentTime = Time.now
   $currentVersion = "v1"
-  $currentRelease = "1.0.0" # X.Y.Z  X-LAST milestone (min. 1), Y-release, Z-increment. Tag starts at 1.0.0, afterwards increase with release.
+  $currentRelease = "1.1.0" # X.Y.Z  X-LAST milestone (min. 1), Y-release, Z-increment. Tag starts at 1.0.0, afterwards increase with release.
 
   private
 	def current_consumer
