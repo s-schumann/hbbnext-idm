404a82cc397cce3a959de6afc85a03fa099d7b91
Sebastian Schumann <seb.schumann@gmail.com>
2013-02-16 19:14:02 +0100

    Added last_login to user-data relation to indicate users last login. Indepedentent of context. The value indicates when user was last time active on this device. Contributes to issue #58.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121023052449) do
+ActiveRecord::Schema.define(:version => 20130216181017) do
 
   create_table "consumers", :force => true do |t|
     t.string   "email"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(:version => 20121023052449) do
     t.string   "name"
     t.string   "uuid"
     t.integer  "role_id"
+    t.datetime "last_login"
   end
 
   create_table "users", :force => true do |t|
