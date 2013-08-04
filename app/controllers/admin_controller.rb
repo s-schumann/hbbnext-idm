804fb30153e36ce123993316b71d1c10f6ac7f3b
Sebastian Schumann <seb.schumann@gmail.com>
2013-08-04 11:28:14 +0200

    Another test with remote IP

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,8 +1,6 @@
 class AdminController < ApplicationController
   def index
     @client_ip = request.remote_ip
-    @remote_ip = request.env["HTTP_X_FORWARDED_FOR"]
-    @my_ip = request.env["HTTP_X_FORWARDED_FOR"] || request.remote_addr
-    @ip_addr = request.env['REMOTE_ADDR']
+    @remote_ip = request.ip
   end
 end
