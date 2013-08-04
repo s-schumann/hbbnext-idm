e05f0bae6de5497762274e04d2fa15114e1b5e77
Sebastian Schumann <seb.schumann@gmail.com>
2013-08-04 11:07:31 +0200

    Modified IP display

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,8 @@
 class AdminController < ApplicationController
   def index
+    @client_ip = request.remote_ip
+    @remote_ip = request.env["HTTP_X_FORWARDED_FOR"]
+    @my_ip = request.env["HTTP_X_FORWARDED_FOR"] || request.remote_addr
+    @ip_addr = request.env['REMOTE_ADDR']
   end
 end
