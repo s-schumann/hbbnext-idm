84813057731d41f15950f23ebd829687abe885a3
Sebastian Schumann <seb.schumann@gmail.com>
2013-08-04 11:51:10 +0200

    Display all remote addresses on Linux

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,6 @@
 class AdminController < ApplicationController
   def index
     @client_ip = request.remote_ip
-    @remote_ip = request.ip
+    @remote_ip =  ip_addresses = `ifconfig -a | grep 'inet ' | grep -v 127.0.0.1 | cut -d':' -f 2 | cut -d' ' -f 1`.split
   end
 end
