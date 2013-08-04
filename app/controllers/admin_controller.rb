06a49c74edc339a5d48cda8e5c70d3c43896c3b0
Sebastian Schumann <seb.schumann@gmail.com>
2013-08-04 12:16:58 +0200

    Show all IPs, added note about not implemented features.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,5 @@
 class AdminController < ApplicationController
+  # GET /admin
   def index
     @client_ip = request.remote_ip
     @remote_ip =  ip_addresses = `ifconfig -a | grep 'inet ' | grep -v 127.0.0.1 | cut -d':' -f 2 | cut -d' ' -f 1`.split
