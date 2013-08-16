92c58118c66d0d4b30eb533f6f950b8cb0d75732
Sebastian Schumann <seb.schumann@gmail.com>
2013-08-16 17:00:38 +0200

    Backported role_id fix for API v2

--- app/controllers/api/v2/devices_controller.rb
+++ app/controllers/api/v2/devices_controller.rb
@@ -3,13 +3,13 @@ module Api
     class DevicesController < ApplicationController
       before_filter :restrict_access
       respond_to :json
-      
+
       # GET /devices.json
       def index
       	@devices = Device.all
         respond_with @devices
       end
-      
+
       # GET /devices/1.json
       def show
         @device = Device.find(params[:id])
@@ -23,34 +23,64 @@ module Api
 
         respond_with @device, @cids
       end
-      
+
       # POST /devices.json
       def create
         @device = Device.new(params[:device])
         @device.uuid = SecureRandom.uuid
+        current_consumer ||= Consumer.find_by_access_token(params[:access_token])
+        @device.created_by = current_consumer.email
         if @device.save
+          set_udr_name
           respond_with @device, status: :created, location: @device
         else
           render json: @device.errors, status: :unprocessable_entity
         end
       end
-      
+
       # PUT /devices/1.json
       def update
-        @device = Device.update(params[:id], params[:device])
+        @device = Device.find(params[:id])
+        if @device.update_attributes(params[:device])
+          if not @device.created_by
+            current_consumer ||= Consumer.find_by_access_token(params[:access_token])
+            @device.created_by = current_consumer.email
+          end
+          set_udr_name
+        end
         respond_with @device
       end
-      
+
       # DELETE /devices/1.json
       def destroy
         @device = Device.destroy(params[:id])
         respond_with @device
       end
-      
+
       private
         def restrict_access
           api_key = Consumer.find_by_access_token(params[:access_token])
           head :unauthorized unless api_key
+        end
+        def set_udr_name
+          @device.udrs.each do |udr|
+            @device.attributes = {
+              :udrs_attributes => [
+                { :id => udr.id,
+                  :name => "#{@device.users.find(udr.user_id).username}:#{@device.display_name}",
+                  :role_id => 4
+                }
+              ]
+            }
+            if not udr.uuid
+              udr.uuid = SecureRandom.uuid
+            end
+            if not udr.created_by
+              current_consumer ||= Consumer.find_by_access_token(params[:access_token])
+              udr.created_by = current_consumer.email
+            end
+          end
+          @device.update_attributes(params[:device])
         end
     end
   end
--- app/controllers/api/v2/users_controller.rb
+++ app/controllers/api/v2/users_controller.rb
@@ -2,14 +2,14 @@ module Api
   module V2
     class UsersController < ApplicationController
       before_filter :restrict_access
-      respond_to :json 
-      
+      respond_to :json
+
       # GET /users.json
       def index
       	@users = User.all
         respond_with @users
       end
-      
+
       # GET /users/1.json
       def show
       	@user = User.find(params[:id])
@@ -20,37 +20,67 @@ module Api
             @cids.push(context.id)
           end
         end
-        
+
         respond_with @user, @cids
       end
-      
+
       # POST /users.json
       def create
         @user = User.new(params[:user])
         @user.uuid = SecureRandom.uuid
+        current_consumer ||= Consumer.find_by_access_token(params[:access_token])
+        @user.created_by = current_consumer.email
         if @user.save
+          set_udr_name
           respond_with @user, status: :created, location: @user
         else
           render json: @user.errors, status: :unprocessable_entity
         end
       end
-      
+
       # PUT /users/1.json
       def update
+        @user = User.find(params[:id])
+        if not @user.created_by
+          current_consumer ||= Consumer.find_by_access_token(params[:access_token])
+          @user.created_by = current_consumer.email
+          @user.save
+        end
         @user = User.update(params[:id], params[:user])
+        set_udr_name
         respond_with @user
       end
-      
+
       # DELETE /users/1.json
       def destroy
         @user = User.destroy(params[:id])
         respond_with @user
       end
-      
+
       private
         def restrict_access
           api_key = Consumer.find_by_access_token(params[:access_token])
           head :unauthorized unless api_key
+        end
+        def set_udr_name
+          @user.udrs.each do |udr|
+            @user.attributes = {
+              :udrs_attributes => [
+                { :id => udr.id,
+                  :name => "#{@user.username}:#{@user.devices.find(udr.device_id).display_name}",
+                  :role_id => 4
+                }
+              ]
+            }
+            if not udr.uuid
+              udr.uuid = SecureRandom.uuid
+            end
+            if not udr.created_by
+              current_consumer ||= Consumer.find_by_access_token(params[:access_token])
+              udr.created_by = current_consumer.email
+            end
+          end
+          @user.update_attributes(params[:user])
         end
     end
   end
